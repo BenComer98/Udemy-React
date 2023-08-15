e0ff913833ee915433baeceb0fa9993aaeff36ba
BenComer98 <benalancomer@gmail.com>
2023-08-15 16:27:43 -0700

    Started new project 6

---
+++ 6_Forms/reading-list-att1/src/components/BookEdit.js
@@ -0,0 +1,7 @@
+import BookShow from './BookShow';
+
+function BookEdit() {
+    return <div>BookEdit</div>;
+}
+
+export default BookEdit;
---
+++ 6_Forms/reading-list-att1/src/components/BookList.js
@@ -0,0 +1,7 @@
+import BookShow from './BookShow'
+
+function BookList() {
+    return <div>BookList</div>;
+}
+
+export default BookList;
